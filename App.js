737bcfc311048f5b8b2446f287ee04ff2fc6ecb9
chetan sobti <sobti.chetan.2009@gmail.com>
2018-12-18 18:45:42 +0530

    initial

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import reducers from './src/reducers'
+import {Header} from './src/components/common'
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+      <View>
+      <Header headerText={'TechStack'}/>
+      </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ListItem.js
@@ -0,0 +1,18 @@
+import React ,{Component} from 'react'
+import {CardSection} from './common'
+import {Text} from 'react-native'
+
+
+class ListItem extends Component{
+
+render()
+{
+	return(
+<CardSection>
+<Text>{this.props.library.title}</Text>
+</CardSection>
+
+);
+}
+}
+export default ListItem
